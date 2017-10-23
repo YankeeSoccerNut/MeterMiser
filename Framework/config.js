e55070c90da412d2d722dead8968cf1b1e4159ef
Michael McFarland <gardengrove422@gmail.com>
2017-10-23 12:22:13 -0400

    Map and Page Updates

---
+++ Framework/config.js
@@ -0,0 +1,3 @@
+const weatherAPIKey = '6f3f23c0f1a2fcb7edee25d08cb9cf62';
+const googleMapsAPIKey ='AIzaSyAD1SSIaneXQl7h7PyrkNOFy8KK7W7D5Zo';
+const honeywellAPIKey ='28JfY56K49mVAE0ODgm018GqVYCeJIuG';
