3ba8523ad7b003a6209b7a654ff6faf8069e1008
Mikayla Kelhofer <mikaylakelhofer@gmail.com>
2017-10-24 07:59:56 -0400

    first draft of bar graph

--- Stackable/scripts.js
+++ Stackable/scripts.js
@@ -1,110 +1,267 @@
 $(document).ready(()=>{
-	var parseDate = d3.time.format("%Y-%m").parse,
-    formatYear = d3.format("02d"),
-    formatDate = function(d) { return "Q" + ((d.getMonth() / 3 | 0) + 1) + formatYear(d.getFullYear() % 100); };
+	// Set the dimensions of the canvas / graph	
+	var margin = {top: 10, right: 20, bottom: 20, left: 60},
+	    width = 400 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
 
-var margin = {top: 10, right: 20, bottom: 20, left: 60},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+	// Adds the svg canvas
+	var svg = d3.select("body") .append("svg")
+	        .attr("width", width + margin.left + margin.right)
+	        .attr("height", height + margin.top + margin.bottom)
+	    .append("g")
+	        .attr("transform",
+	              "translate(" + margin.left + "," + margin.top + ")");
 
-var y0 = d3.scale.ordinal()
-    .rangeRoundBands([height, 0], .2);
+	// Define Ranges
+	// var x = function(d) {return d.location;};
+	var xScale = d3.scaleBand().range([0,width]).paddingInner(0.2).paddingOuter(0.1);
+	
+	// var y = function(d) {return d.value};
+	var ySplitScale = d3.scaleLinear();
+	var yStackScale = d3.scaleLinear();
 
-var y1 = d3.scale.linear();
+	// var settingRow = function(d) {return d.status};
+	var settingRowScale = d3.scaleBand().range([height,0]).paddingInner(0.25);
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1, 0);
+	// var storeHoursRow = function(d) {return d.}
+	var storeHoursRowScale = d3.scaleBand().range([0, height]).paddingInner(0.2);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-    .tickFormat(formatDate);
+	// var colorScale = d3.scaleOrdinal().
 
-var nest = d3.nest()
-    .key(function(d) { return d.group; });
 
-var stack = d3.layout.stack()
-    .values(function(d) { return d.values; })
-    .x(function(d) { return d.date; })
-    .y(function(d) { return d.value; })
-    .out(function(d, y0) { d.valueOffset = y0; });
+	// Define Domains
+	xScale.domain(['East Cobb', 'West Cobb', 'Roswell']);
+	settingRowScale.domain(['Hold - Permanent','Hold - Temporary', 'Scheduled', 'Off']);
 
-var color = d3.scale.category10();
+	// Define the axes
+	var xAxis = d3.axisBottom(xScale).ticks(3);
+	var yAxis = d3.axisLeft(settingRowScale);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	// Get the data
+	d3.json('https://api.myjson.com/bins/1grppr', function(error,data){
+		// console.log(data);
+		var dataFormated = formatJSON(data);
+		// console.log(dataFormated);
+		var dataBySetting = nestedSetting(dataFormated);
+		// console.log(dataBySetting);
+		// console.log(d3.max(dataBySetting, function(d,i) {
+		// 	for(let i = 0; i< d.values.length; i++){
+		// 		return d.values[i].value;	
+		// 	}}))
+		var dataByStoreHours = nestedHours(dataFormated);
+		// console.log(dataByStoreHours);
+		// console.log(d3.max(dataByStoreHours, function(d,i) {
+		// 	for(let i = 0; i< d.values.length; i++){
+		// 		return d.values[i].value;	
+		// 	}}))
+		var dataBySettingPerStoreHours = nestedHoursSetting(dataFormated);
+		// console.log(dataBySettingPerStoreHours);
+		// console.log(d3.max(dataBySettingPerStoreHours, function(d,i,j) {
+		// 	for(let i = 0; i< d.values.length; i++){
+		// 		for(let j =0; j < d.values[i].values.length; j++)
+		// 		return d.values[i].values[j].value;	
+		// 	}}))
+		var dataByLocation = nestedLocation(dataFormated);
+		// console.log(dataByLocation);
+		// console.log(d3.max(dataByLocation, function(d) {return d.value;}))
 
-d3.tsv("data.tsv", function(error, data) {
+		ySplitScale.domain([0,d3.max(dataBySetting, function(d) {
+				return d3.max(d.values, function(d){
+					return d.value;
+				})})])
+		.range([settingRowScale.bandwidth(),0]);
 
-  data.forEach(function(d) {
-    d.date = parseDate(d.date);
-    d.value = +d.value;
-  });
+		yStackScale.domain([0,d3.max(dataByLocation, function(d) {return d.value})])
 
-  var dataByGroup = nest.entries(data);
+		svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  stack(dataByGroup);
-  x.domain(dataByGroup[0].values.map(function(d) { return d.date; }));
-  y0.domain(dataByGroup.map(function(d) { return d.key; }));
-  y1.domain([0, d3.max(data, function(d) { return d.value; })]).range([y0.rangeBand(), 0]);
 
-  var group = svg.selectAll(".group")
-      .data(dataByGroup)
-    .enter().append("g")
-      .attr("class", "group")
-      .attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
+		var gRow = svg.append('g').attr('class', 'rows')
+            .selectAll('.row').data(dataBySetting)
+        .enter().append('g')
+            .attr('class', 'row')
+            .attr('transform', function(d) { return 'translate(0,' + settingRowScale(d.key) + ')'; });
 
-  group.append("text")
-      .attr("class", "group-label")
-      .attr("x", -6)
-      .attr("y", function(d) { return y1(d.values[0].value / 2); })
-      .attr("dy", ".35em")
-      .text(function(d) { return "Group " + d.key; });
+        gRow.append('text').attr('class', 'title')
+        .attr('dx', '-0.34em')
+        .text(function(d) {return d.key});
 
-  group.selectAll("rect")
-      .data(function(d) { return d.values; })
-    .enter().append("rect")
-      .style("fill", function(d) { return color(d.group); })
-      .attr("x", function(d) { return x(d.date); })
-      .attr("y", function(d) { return y1(d.value); })
-      .attr("width", x.rangeBand())
-      .attr("height", function(d) { return y0.rangeBand() - y1(d.value); });
+        var bars = gRow.append('g').attr('class', 'bars').style("fill", function(d){
+        	if(d.key == 'Off'){
+        		return '#BDC3C7'
+        	}else if(d.key == 'Scheduled'){
+        		return '#27AE60'
+        	}else if(d.key == 'Hold - Temporary'){
+        		return '#F1C40F'
+        	}else if (d.key == 'Hold - Permanent'){
+        		return '#E74C3C'
+        	}
+        });
 
-  group.filter(function(d, i) { return !i; }).append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + y0.rangeBand() + ")")
-      .call(xAxis);
+        bars.selectAll(".bar-underlying").data(function(d) {return d.values;})
+        	.enter().append('rect')
+        		.attr('class', 'bar bar-underlying')
+	            .attr('x', function(d) {return xScale(d.key);})
+	            .attr('y', 0)
+	            .attr('width', xScale.bandwidth())
+	            .attr('height', function(d) { return ySplitScale.range()[1];})
+	            .style('fill', '#fff');
 
-  d3.selectAll("input").on("change", change);
 
-  var timeout = setTimeout(function() {
-    d3.select("input[value=\"stacked\"]").property("checked", true).each(change);
-  }, 2000);
 
-  function change() {
-    clearTimeout(timeout);
-    if (this.value === "multiples") transitionMultiples();
-    else transitionStacked();
-  }
+        bars.selectAll(".bar-overlying").data(function(d) {return d.values;})
+        	.enter().append('rect')
+        		.attr('class', 'bar bar-overlying')
+	            .attr('x', function(d) {return xScale(d.key);})
+	            .attr('y', function(d) {return ySplitScale(d.value);})
+	            .attr('width', xScale.bandwidth())
+	            .attr('height', function(d) { return ySplitScale(0) - ySplitScale(d.value);})
+	 //            
 
-  function transitionMultiples() {
-    var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", function(d) { return "translate(0," + y0(d.key) + ")"; });
-    g.selectAll("rect").attr("y", function(d) { return y1(d.value); });
-    g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2); })
-  }
+		
+	});
 
-  function transitionStacked() {
-    var t = svg.transition().duration(750),
-        g = t.selectAll(".group").attr("transform", "translate(0," + y0(y0.domain()[0]) + ")");
-    g.selectAll("rect").attr("y", function(d) { return y1(d.value + d.valueOffset); });
-    g.select(".group-label").attr("y", function(d) { return y1(d.values[0].value / 2 + d.values[0].valueOffset); })
-  }
-});
+
+
+
+
+
+	function formatJSON(rawData){
+		var dataFormated = []
+		rawData.forEach(function(d,i){
+			dataFormated[i] = {};
+			dataFormated[i].location = LocationName(d.thermostatId);
+			dataFormated[i].dateTimeInfo = new DateTime(d.created);
+			dataFormated[i].status = determineStatus(d.statusHeat, d.systemSwitchPos);
+		})
+		return dataFormated;
+	}
+
+
+	function LocationName(thermostatId){
+		if(thermostatId == 581279){
+			return 'Roswell';
+		}
+		else if(thermostatId == 594679){
+			return 'East Cobb';
+		}
+		else if(thermostatId == 597696){
+			return 'West Cobb';
+		}
+		else{
+			return 'Not Valid Id';
+		}
+	}
+
+	function determineStatus(statusHeat,systemSwitchPos){
+		if(systemSwitchPos == 1 || systemSwitchPos == 3){
+			if(statusHeat == 0){
+				return 'Scheduled';
+			}else if(statusHeat == 1){
+				return 'Hold - Temporary';
+			}else if(statusHeat == 2){
+				return 'Hold - Permanent'
+			}
+		}
+		else if(systemSwitchPos == 2){
+			return 'Off';
+		}else{
+			return 'error';
+		}
+	}
+	var location_order = ['East Cobb', 'West Cobb', 'Roswell'];
+	var setting_order = ['Off','Hold - Permanent','Hold - Temporary', 'Scheduled'];
+	function nestedLocation(dataFormated){
+		return d3.nest()
+			.key(function(d) {return d.location}).sortKeys(function(a,b) { return location_order.indexOf(a) - location_order.indexOf(b); })
+			.rollup(function(leaves) { return leaves.length; })
+			.entries(dataFormated);
+	}
+
+	function nestedSetting(dataFormated){
+		return d3.nest()
+			.key(function(d) {return d.status}).sortKeys(function(a,b) { return setting_order.indexOf(a) - setting_order.indexOf(b); })
+			.key(function(d) {return d.location}).sortKeys(function(a,b) { return location_order.indexOf(a) - location_order.indexOf(b); })
+			.rollup(function(leaves) { return leaves.length; })
+			.entries(dataFormated);
+	}
+
+	function sumNestedSetting(dataFormated){
+		return d3.nest()
+			.key(function(d) {return d.status}).sortKeys(function(a,b) { return setting_order.indexOf(a) - setting_order.indexOf(b); })
+			.rollup(function(leaves) { return leaves.length; })
+			.entries(dataFormated);
+	}
+
+	function nestedHours(dataFormated){
+		return d3.nest()
+			.key(function(d) {return d.dateTimeInfo.duringOperationHours}).sortKeys(d3.ascending)
+			.key(function(d) {return d.location}).sortKeys(function(a,b) { return location_order.indexOf(a) - location_order.indexOf(b); })
+			.rollup(function(leaves) { return leaves.length; })
+			.entries(dataFormated);
+	}
+
+
+
+	function nestedHoursSetting(dataFormated){
+		return d3.nest()
+			.key(function(d) {return d.dateTimeInfo.duringOperationHours}).sortKeys(d3.ascending)
+			.key(function(d) {return d.status}).sortKeys(function(a,b) { return setting_order.indexOf(a) - setting_order.indexOf(b); })
+			.key(function(d) {return d.location}).sortKeys(function(a,b) { return location_order.indexOf(a) - location_order.indexOf(b); })
+			.rollup(function(leaves) { return leaves.length; })
+			.entries(dataFormated);
+	}
+
+	
+	function DateTime(dateString){
+		this.timeStamp = Date.parse(dateString);
+		this.dateComplete = new Date(dateString);
+		this.year = this.dateComplete.getUTCFullYear();
+		this.month = this.dateComplete.getUTCMonth();
+		this.day = this.dateComplete.getUTCDay();
+		this.date = this.dateComplete.getUTCDate();
+		this.hour = this.dateComplete.getUTCHours();
+		this.minute = this.dateComplete.getUTCMinutes();
+		this.duringOperationHours = compareToStoreHours(this.hour);
+	}
+
+	function compareToStoreHours(hour){
+		var storeHours = {
+			open: 14, //9am (shifted 5 for UTC)
+			close: 23  //6pm (shifted 5 for UTC)
+		}
+		if(hour >= storeHours.open && hour < storeHours.close){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	function filterByTimeframe(data, timeframe){
+		var dataByTimeframe = [];
+		var mostRecent = d3.max(dataFormated, function(d){return d.dateTimeInfo.timeStamp});
+		var timeframe;
+		if(domain == 'oneDay'){
+			timeframe = 24*60*60*1000;
+		}else if(domain == 'oneWeek'){
+			timeframe = 24*60*60*1000*7;
+		}else{//month
+			timeframe = 24*60*60*1000*30;
+		}
+		data.forEach(function(d){
+			if(d.dateTimeInfo.timeStamp >= mostRecent - timeframe){
+				dataByTimeframe.push(d);
+			}
+		})
+		return dataByTimeframe;		
+	}
+
+
+
 
 
 	
-})
+})//doc ready
